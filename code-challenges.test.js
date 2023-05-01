13193362f4678ac9b68d3f93dc7e8f917ca6c877
Valeria <valeriacasillas123@yahoo.com>
2023-05-01 08:49:05 -0700

    week6

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,37 +10,192 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+//1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
-// a) Create a test with an expect statement using the variable provided.
 
+// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+// // a) Create a test with an expect statement using the variable provided.
+  
+describe("sentenceJoiner" , () =>{
+    it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", ()=>{
+      expect(sentenceJoiner(people)).toEqual("Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee.")
+    })
+  })
+
+
+  // FAIL  ./code-challenges.test.js
+  // sentenceJoiner
+  //   ✕ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (3 ms)
+
+  // ● sentenceJoiner › takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+  //   ReferenceError: sentenceJoiner is not defined
+
+  //     25 | describe("sentenceJoiner" , () =>{
+  //     26 |     it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", ()=>{
+  //   > 27 |       expect(sentenceJoiner(people)).toEqual("Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee.")
+  //        |       ^
+  //     28 |     })
+  //     29 |   })
+  //     30 |     
+
+  //     at Object.expect (code-challenges.test.js:27:7)
 
 // b) Create the function that makes the test pass.
+// Use .map and .split & toUpperCase.
+// Use split to split the string into seperate words and put it into a new array that'll then get uppercased. It should return the n
 
-// --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+const sentenceJoiner = (variable) => {
+  return variable.map((newPeople)=>{
+    let variable1 = newPeople.split('')
+    let variable2 = variable1.toUpperCase
+    return variable2
 
-// a) Create a test with an expect statement using the variables provided.
+  })
+}
+const newPeople = sentenceJoiner(people);
+console.log(newPeople)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
-// b) Create the function that makes the test pass.
-
-// --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+describe("arrayDivider", ()=>{
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", ()=>{
+    expect(arrayDivider(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(arrayDivider(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+
+
+// FAIL  ./code-challenges.test.js
+// arrayDivider
+//   ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (1 ms)
+
+// ● arrayDivider › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+//   ReferenceError: arrayDivider is not defined
+
+//     85 | describe("arrayDivider", ()=>{
+//     86 |   it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", ()=>{
+//   > 87 |     expect(arrayDivider(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+//        |     ^
+//     88 |     expect(arrayDivider(hodgepodge2)).toEqual([ 2, 1, -1 ])
+//     89 |   })
+//     90 | })
+
+//     at Object.expect (code-challenges.test.js:87:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.533 s, estimated 1 s
+
+// b) Create the function that makes the test pass.
+ // Use filter to filter out anything out of the array that isn't a number & map to create a new aray out of the filtered array. With the new filtered array i'll divide each number by 3 and put it into a new array. 
+const arrayDivider = (array) => {
+  const newArray = array.filter(item =>typeof item === 'number');
+  const newArray2 = newArray.map(num => num %3 );
+  return newArray2
+};
+console.log(arrayDivider(hodgepodge1));
+console.log(arrayDivider(hodgepodge2));
+
+
+
+
+
+
+
+
+
+
+
+//3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+
+// a) Create a test with an expect statement using the variables provided.
+
+describe("arrayCuber",()=>{
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.",()=>{
+    expect(cubeAndSum1).toEqual(99)
+    expet(cubeAndSum2).toEqual(1125)
+  })
+})
+
+
+
+// FAIL  ./code-challenges.test.js
+// arrayDivider
+//   ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (3 ms)
+// arrayCuber
+//   ✕ takes in an array of numbers and returns the sum of all the numbers cubed. (3 ms)
+
+// ● arrayCuber › takes in an array of numbers and returns the sum of all the numbers cubed.
+
+//   expect(received).toEqual(expected) // deep equality
+
+//   Expected: 99
+//   Received: [2, 3, 4]
+
+//     147 | describe("arrayCuber",()=>{
+//     148 |   it("takes in an array of numbers and returns the sum of all the numbers cubed.",()=>{
+//   > 149 |     expect(cubeAndSum1).toEqual(99)
+//         |                         ^
+//     150 |     expet(cubeAndSum2).toEqual(1125)
+//     151 |   })
+//     152 | })
+
+//     at Object.toEqual (code-challenges.test.js:149:25)
+
+// Test Suites: 1 failed, 1 total
+
+
+
 // b) Create the function that makes the test pass.
+
+
+
+// So to get cubeAndSum1 to equal 99... I had to google how a cubed number works. 
+//I should multiple each number in the array by 3 of itself so 2x2x2 =8, 3x3x3 = 27 and 4x4x4= 64  and then add all of them to the sum which should equal 99.
+// So I used a for loop so that it can just loop over each number and multiply each number by itself 3 times and add the sum into a new array. The starting sum should be 0 and at the end it'll be the sum of each number cubed and added up.
+
+//
+ const arrayCuber = (array)=>{
+  let cubedSum = 0;
+  for(let i = 0;i <array.length; i++){
+    cubedSum += array[i]**3;
+  } 
+  return cubedSum
+ }
+ console.log(arrayCuber(cubeAndSum1));
+ console.log(arrayCuber(cubeAndSum2));
--- interview-questions.md
+++ interview-questions.md
@@ -8,9 +8,35 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort and a model called Student. The cohort model has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: The foreign key will be called whatever you want it to be called & it would be added in the student model because each cohort should have many student but each student cannot have many cohorts. So to fix the mistake of not adding a FK you can do add_foreign_key which should add a foreign key.
 
 Researched answer:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 2. Which RESTful routes must always be passed params? Why?
 
@@ -18,26 +44,96 @@ Your answer:
 
 Researched answer:
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 3. Name three rails generator commands. What is created by each?
 
 Your answer:
 
-Researched answer:
+Rails generate model: creates a new file called model in the apps/models directory. Models essentially represent all data and provide an interface , like one you'd see while online shopping to interact with the databse table aka whats in stock.
+
+Rails generate migrate: Creates a new file in db/migrate and allows you to modify database schema and lets you modify the data types and add/remove. Such as removing or adding more items to an online store.
+
+Rails generate channel: creates a action cabel channel in apps/channel . What this command does is tht it creates a channel class that has methods that can for example handle subscription request and serve as broadcasting msgs to said subscribers
+
+Researched answer: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET" location: /students
+GET - Index /show method
+POST- Create methods 
+PATCH- Update/replace methods
+PUT- update/modify methodss
+DELETE- destroy methods
+
+
+action: "GET" location: /students  
+//Index
+// It'll show a list of all students
+
 
 action: "POST" location: /students
+//Create
+//This allows us to create a new student record with certain parameteres , saving it to our database.
 
 action: "GET" location: /students/new
+//It says it there , new.
+// This allows us to create a new student by making a form .
 
 action: "GET" location: /students/2
+//thisw will just call upon show.
+// This just displays the student with ID 2
 
 action: "GET" location: /students/2/edit
+//It says it there that it will call upon edit.
+//This should get the student ID 2 and display it for us to edit.
+
+
 
 action: "PATCH" location: /students/2
+//it will call upon update.
+//This should allow us to update the student with ID of 2 only and save it to database.
 
 action: "DELETE" location: /students/2
+//It would call  destroy
+// This should delete the student with ID of 2
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
